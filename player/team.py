d3c2b5deca7f36ecf98dd71422e24f0e26c2d333
bertalay <alu@imsa.edu>
2019-02-16 16:05:34 -0500

    Put in phase structure

--- player/team.py
+++ player/team.py
@@ -22,26 +22,27 @@ class Team(object):
         on the wiki. team_size, although passed to you as a parameter, will
         always be 4.
         """
-        self.board = initial_board
-        self.visited = []
-        self.remembered_board = [[0 for i in range(len(initial_board[0]))] for j in range(len(initial_board))]
-
-        
         self.team_size = team_size
         self.company_info = company_info
+        self.board = initial_board
+        self.visited = []
+        self.dimensions = [len(initial_board), len(initial_board[0])]
+        self.remembered_board = [[0 for i in range(len(initial_board[0]))] for j in range(len(initial_board))]
+        self.phase = ["spreading" for i in range(self.team_size)]
+        self.turnCount = 0
+        
+        
 
         self.team_name = "this is fine"
-        print(self.team_name)
-        for s in self.board:
-            acc = ""
-            for k in s:
-                acc += str(k.get_booth()) + " "
-            print(acc)
-         
-        print(self.board[1][2].get_booth())
-        print(self.company_info)
 
     def step(self, visible_board, states, score):
-
-
-        return [Direction.UP, Direction.LEFT, Direction.DOWN, Direction.RIGHT]
+        returnList = [Direction.UP, Direction.UP, Direction.UP, Direction.UP]
+        print(self.turnCount)
+        self.turnCount += 1
+        for i in range(self.team_size):
+            if self.phase == "spreading":
+                i = i + 1 - 1
+                #implmement
+            
+            #same sort of structure for each of these phases
+        return returnList
